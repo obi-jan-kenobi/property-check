c0f94c3c954e9ad25a49943bb285964384c5fa5a
Jan Sziegaud <j.sziegaud@gmail.com>
2017-11-20 16:53:07 +0100

    typeguards added

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
-"use strict"
+"use strict";
 
 const property = (...generators) => (prop, iterations = 100) => {
-  const gens = generators.map(gen => gen())
-  for (let i = 0; i < iterations; i++) {
-    const next = gens.map(gen => gen.next())
-    if (!prop(...next)) throw new Error(`test failed with ${JSON.stringify(next)}`)
+  for (let generator of generators) {
+    if (
+      typeof generator !== "function" ||
+      typeof generator.prototype.next !== "function"
+    )
+      throw new TypeError(`${generator} is not a GeneratorFunction`);
   }
-  gens.map(gen => gen.next(true))
-}
+  if (typeof prop !== "function")
+    throw new TypeError(`${prop} must a function`);
+  const gens = generators.map(gen => gen());
+  for (let i = 0; i < iterations; i++) {
+    const next = gens.map(gen => gen.next());
+    if (!prop(...next))
+      throw new Error(`test failed with ${JSON.stringify(next)}`);
+  }
+  gens.map(gen => gen.next(true));
+};
 
 const sample = generator => {
-  const ret = new Array(6)
-  const values = generator()
-  return ret.fill().map(_ => values.next())
+  const values = generator();
+  return new Array(6).fill().map(_ => values.next());
+};
+
+function* charGen() {
+  while (true) yield String.fromCharCode(Math.round(Math.random() * 65535));
 }
 
-function * charGen () {
-  while (true) yield String.fromCharCode(Math.round(Math.random() * 65535))
+function* intGen() {
+  while (true) yield Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
 }
 
-function * intGen () {
-  while (true) yield Math.round(Math.random() * Number.MAX_SAFE_INTEGER)
-}
-
-function * asyncGen () {
-  let idx = 1
+function* asyncGen() {
+  let idx = 1;
   while (true) {
-    yield new Promise((resolve) => setTimeout(() => resolve(idx++), 500))
+    yield new Promise(resolve => setTimeout(() => resolve(idx++), 500));
   }
 }
 
-module.exports =
-  { property
-  , generators:
-    { intGen
-    , charGen
-    }
-  , sample
-  }
+module.exports = {
+  property,
+  generators: {
+    intGen,
+    charGen
+  },
+  sample
+};
