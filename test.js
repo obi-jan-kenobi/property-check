208c93106de32b46b544a6b05fe9f9bf7c3e1eb1
Jan Sziegaud <j.sziegaud@gmail.com>
2017-11-20 17:20:36 +0100

    testcases added for typeguards

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
-"use strict"
+"use strict";
 
-const assert = require("assert")
-const {property, generators: gen, sample} = require(".")
+const assert = require("assert");
+const { property, generators: gen, sample } = require(".");
 
-assert.doesNotThrow(() => property(gen.intGen)(x => x < x + 1))
-assert.throws(() => property(gen.intGen)(x => x > x + 1))
-
+// doesnt throw with proper usage & valid property
+assert.doesNotThrow(() => property(gen.intGen)(x => x < x + 1));
 assert.doesNotThrow(() =>
-  property(gen.intGen, gen.intGen, gen.intGen)((x, y, z) =>
-    (x + y) + z == x + (y + z)))
+  property(gen.intGen, gen.intGen, gen.intGen)(
+    (x, y, z) => x + y + z == x + (y + z)
+  )
+);
+
+// does throw with invalid property
+assert.throws(() => property(gen.intGen)(x => x > x + 1));
+
+// does throw with improper usage
+assert.throws(() => property(x => x)(x => x == x));
+assert.throws(() => property(gen.charGen, "string")(x => x == x));
